7fa90d7fdced30cdc1f0f75dac4f24f9d8261bce
Savosin Anton <anle@list.ru>
2022-03-12 22:12:30 +0300

    дз 2_2 - доработка2 - задача 2

--- src/main/kotlin/ru/netology/hw2_2/WallService.kt
+++ src/main/kotlin/ru/netology/hw2_2/WallService.kt
@@ -2,21 +2,22 @@ package ru.netology.hw2_2
 
 object WallService {
     private var posts = emptyArray<Post>()
+    private var lastPostID = 0
 
     fun add(post: Post): Post {
-        posts += if (posts.isNotEmpty())
-            post.copy(id = (posts.last()).id + 1)
-        else post.copy(id = 1)
-        return posts.last()
+        if (lastPostID != 0) lastPostID += 1 else lastPostID = 1
+        val nextId =  lastPostID
+        val identifiedPost = post.copy(id = nextId)
+        posts += post
+        return identifiedPost
     }
 
     fun update (post: Post): Boolean {
-        var isPostUpdated = false
         for ((index, arrPost) in posts.withIndex())
             if (arrPost.id == post.id) {
-                posts[index] = post
-                isPostUpdated = true
+                posts[index] = post.copy(ownerId = arrPost.ownerId, date = arrPost.date)
+                return true
             }
-        return isPostUpdated
+        return false
     }
 }
